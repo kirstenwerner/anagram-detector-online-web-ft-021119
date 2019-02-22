0640b1998ed28d86bd7a1b083d9c337465d2ccff
Kirsten Werner <kirstenmwerner@gmail.com>
2019-02-22 22:27:39 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
-# Your code goes here!
+# require 'pry'
+class Anagram
+  
+  attr_accessor :word 
+
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(array_of_anagrams)
+    anagram_array = []
+    sorted_word = @word.chars.sort
+    array_of_anagrams.each do |anagram|
+      sorted_anagram = anagram.chars.sort
+      if sorted_anagram.eql?(sorted_word) == true 
+        anagram_array << anagram 
+      end
+      # binding.pry
+    end 
+    anagram_array
+  end 
+  
+  
+end 
+
+
+# listen = Anagram.new("listen")
+# listen.match(%w(enlists google inlets banana))
